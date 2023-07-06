754e9b28894a5cdefc3a48993a6ce853a9ede620
Fabio Leo <s257527@studenti.polito.it>
2023-07-06 20:27:37 +0200

    Update-test

--- Projects/Raffinamento/test/test_Triangle_class.hpp
+++ Projects/Raffinamento/test/test_Triangle_class.hpp
@@ -12,15 +12,9 @@ using namespace ProjectLibrary;
 TEST(InitTest, TriStructTest)
 {
     unsigned int triID = 0;
-    array<unsigned int, 3> vertIDs = {0, 1, 2};
-    array<unsigned int, 3> edgeIDs = {0, 1, 2};
-    array<Vector2d, 3> triCoords = {
-            Vector2d(0, 0),
-            Vector2d(1, 0),
-            Vector2d(1, 1)
-    };
+    array<Vector2d, 3> triCoords = {Vector2d(0, 0), Vector2d(1, 0), Vector2d(1, 1)};
 
-    TriStruct triData(triID, vertIDs, triCoords, edgeIDs);
+    TriStruct triData(triID, triCoords);
 
 
     TriangularMesh mesh("D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\InitMeshTest\\Cell0Ds.csv",
@@ -36,10 +30,8 @@ TEST(TriangleTest, TriangleAreaTest)
 {
     unsigned int triID = 0;
     array<Vector2d, 3> triCoords = {Vector2d(0, 0), Vector2d(1, 0), Vector2d(1, 1)};
-    array<unsigned int, 3> vertIDs = {0, 1, 2};
-    array<unsigned int, 3> edgeIDs = {0, 1, 2};
 
-    TriStruct triData(triID, vertIDs, triCoords, edgeIDs);
+    TriStruct triData(triID, triCoords);
     Triangle tri(triData);
 
     EXPECT_EQ(tri.Area(), 0.5);
--- Projects/Raffinamento/test/test_TriangularMesh_class.hpp
+++ Projects/Raffinamento/test/test_TriangularMesh_class.hpp
@@ -57,19 +57,15 @@ TEST(TriangleVector, TriangleVectorTest)
 
     unsigned int triID0 = 0;
     array<Vector2d, 3> triCoords0 = {Vector2d(0, 0), Vector2d(1, 0), Vector2d(1, 1)};
-    array<unsigned int, 3> vertIDs0 = {0, 1, 2};
-    array<unsigned int, 3> edgeIDs0 = {0, 1, 2};
 
-    TriStruct triData0(triID0, vertIDs0, triCoords0, edgeIDs0);
+    TriStruct triData0(triID0, triCoords0);
     Triangle tri0(triData0);
     triangles0.triangles.push_back(tri0);
 
     unsigned int triID1 = 1;
     array<Vector2d, 3> triCoords1 = {Vector2d(0, 0), Vector2d(1, 1), Vector2d(0, 1)};
-    array<unsigned int, 3> vertIDs1 = {0, 2, 3};
-    array<unsigned int, 3> edgeIDs1 = {2, 3, 4};
 
-    TriStruct triData1(triID1, vertIDs1, triCoords1, edgeIDs1);
+    TriStruct triData1(triID1, triCoords1);
     Triangle tri1(triData1);
     triangles0.triangles.push_back(tri1);
 
@@ -91,6 +87,47 @@ TEST(AdjacentTrianglesTest, GetAdjacentTrianglesTest)
                         "D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell2Ds.csv");
 
     ASSERT_TRUE(mesh.GetAdjacentTriangles(10)[0] == 5 || mesh.GetAdjacentTriangles(10)[1] == 6);
+}
+
+TEST(EdgeTest, EdgeLengthTest)
+{
+    TriangularMesh mesh("D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell0Ds.csv",
+                        "D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell1Ds.csv",
+                        "D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell2Ds.csv");
+
+    double Length0 = mesh.EdgeLength(4);
+    double expectedLength0 = 0.44721359549995798;
+
+    double Length1 = mesh.EdgeLength(11);
+    double expectedLength1 = 0.44721359549995798;
+
+    double Length2 = mesh.EdgeLength(16);
+    double expectedLength2 = 0.4;
+
+    ASSERT_DOUBLE_EQ(Length0, expectedLength0);
+    ASSERT_DOUBLE_EQ(Length1, expectedLength1);
+    ASSERT_DOUBLE_EQ(Length2, expectedLength2);
+}
+
+TEST(EdgeTest, EdgeMaxTest)
+{
+    TriangularMesh mesh("D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell0Ds.csv",
+                        "D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell1Ds.csv",
+                        "D:\\PoliTo\\Matematica\\3-Anno\\Programmazione_e_calcolo_scientifico\\Progetto\\Progetto_PCS_FMV\\Projects\\Raffinamento\\Dataset\\GeneralTest\\Cell2Ds.csv");
+
+    unsigned int maxEdge0 = mesh.MaxEdge(2);
+    unsigned int expectedmaxEdge0 = 1;
+
+    unsigned int maxEdge1 = mesh.MaxEdge(5);
+    unsigned int expectedmaxEdge1 = 14;
+
+    unsigned int maxEdge2 = mesh.MaxEdge(8);
+    unsigned int expectedmaxEdge2 = 16;
+
+
+    ASSERT_DOUBLE_EQ(maxEdge0, expectedmaxEdge0);
+    ASSERT_DOUBLE_EQ(maxEdge1, expectedmaxEdge1);
+    ASSERT_DOUBLE_EQ(maxEdge2, expectedmaxEdge2);
 }
 
 #endif // test_TriangularMesh_class_H
